3842fd44777dd123ae4646ddc9829d2f6153a8f6
Dbooze10 <Desmondbooze1@gmail.com>
2023-07-15 17:48:22 -0400

    added a post and header page

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './Post.js';
+import Header from './Header.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <main>
-    <header>
-      <a href="" className="logo">MyBlog</a>
-      <nav>
-        <a href="">Login</a>
-        <a href="">Sign-Up</a>
-      </nav>
-    </header>
-    <div className="post">
-      <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:eco,dpr_2.0/rockcms/2023-07/lebron-james-family-espy-mc-230713-eaaa5c.jpg" alt=""></img>
-      <div className="texts">
-      <h2>LeBron James poses with his 3 kids and wife on the red carpet at the ESPYS</h2>
-      <p>The NBA pro brought his wife, Savannah James, and three children to the annual awards show in Los Angeles, and they posed for some rare family photos on the red carpet.</p>
-      </div>
-    </div>
-    <div className="post">
-      <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:eco,dpr_2.0/rockcms/2023-07/lebron-james-family-espy-mc-230713-eaaa5c.jpg" alt=""></img>
-      <div className="texts">
-      <h2>LeBron James poses with his 3 kids and wife on the red carpet at the ESPYS</h2>
-      <p>The NBA pro brought his wife, Savannah James, and three children to the annual awards show in Los Angeles, and they posed for some rare family photos on the red carpet.</p>
-      </div>
-    </div>
-    <div className="post">
-      <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:eco,dpr_2.0/rockcms/2023-07/lebron-james-family-espy-mc-230713-eaaa5c.jpg" alt=""></img>
-      <div className="texts">
-      <h2>LeBron James poses with his 3 kids and wife on the red carpet at the ESPYS</h2>
-      <p>The NBA pro brought his wife, Savannah James, and three children to the annual awards show in Los Angeles, and they posed for some rare family photos on the red carpet.</p>
-      </div>
-    </div>
-   </main>
+    <BrowserRouter>
+      <Routes>
+        <Route index element= {
+          <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+        } />
+        <Route path={'/login'} element={
+          <main>
+            <Header />
+            <div>login</div>
+          </main>
+        } />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:eco,dpr_2.0/rockcms/2023-07/lebron-james-family-espy-mc-230713-eaaa5c.jpg" alt=""></img>
+            </div>
+            <div className="texts">
+                <h2>LeBron James poses with his 3 kids and wife on the red carpet at the ESPYS</h2>
+                <p className="info">
+                    <a className="author">Desmond Booze</a>
+                    <time>07-15-2023 1:02 PM</time>
+                </p>
+                <p className="summary">The NBA pro brought his wife, Savannah James, and three children to the annual awards show in Los Angeles, and they posed for some rare family photos on the red carpet.</p>
+            </div>
+      </div>
+    );
+}
