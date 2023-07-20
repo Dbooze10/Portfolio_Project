b0c2fd7d6291592ae73faaa7b87fc582bea584b9
Dbooze10 <Desmondbooze1@gmail.com>
2023-07-19 20:31:44 -0400

    added models folder

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User')
 const app = express();
 
-app.post('/register', (req, res) => {
-    res.json('test ok');
-    
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://dbooze10:<Des22023>@cluster0.rsix6fu.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 });
 
 app.listen(3000);
