480cabcfb29369c78ff11a4117cbee8130d16df8
Dbooze10 <Desmondbooze1@gmail.com>
2023-07-23 11:18:26 -0400

    add editpost and editor.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,17 +1,21 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css'
 import {useState} from "react";
+import { Navigate } from "react-router-dom";
+import Editor from "../Editor"
 
 export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
     const [redirect, setRedirect] = useState(false);
     async function createNewPost(ev) {
         const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
+        data.set('file', files[0]);
         ev.preventDefault();
         const response = await fetch('http://localhost:4000/post', {
             method: 'POST', 
@@ -31,8 +35,8 @@ export default function CreatePost() {
         <form onSubmit={createNewPost}>
             <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)}></input>
             <input type="summary" placeholder={'Summary'} value={summary} onChange={ev => setSummary(ev.target.value)}></input>
-            <input type="file"></input>
-            <ReactQuill value={content} onChange={newValue => setContent(newValue)} module={modules}/>
+            <input type="file" onChange={ev => setFiles(ev.target.files)}></input>
+            <Editor value={content} onChange={setcontent} />
             <button style={{marginTop: '5px'}}>Create Post</button>
         </form>
     )
